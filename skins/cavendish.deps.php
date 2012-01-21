8a0a7bc4eceff6a36ef6c8474b76af9a8a75efa7
Shinya Maeyama <merikonjatta@gmail.com>
2012-01-22 05:31:53 +0900

    Trying different version of cavendish skin

--- skins/cavendish.deps.php
+++ skins/cavendish.deps.php
@@ -3,9 +3,10 @@
 // MonoBook.php is compiled, working around a bug in the APC opcode
 // cache on PHP 5, where cached code can break if the include order
 // changed on a subsequent page view.
-// see http://lists.wikimedia.org/pipermail/wikitech-l/2006-January/021311.html
+// see http://mail.wikipedia.org/pipermail/wikitech-l/2006-January/033660.html
 
 if ( ! defined( 'MEDIAWIKI' ) )
 	die( 1 );
-
-require_once( dirname( dirname( __FILE__ ) ) . '/includes/SkinTemplate.php');
+global $IP;
+require_once( $IP .'/includes/SkinTemplate.php');
+require_once( $IP . '/skins/MonoBook.php');
